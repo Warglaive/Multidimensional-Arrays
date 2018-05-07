25eb825f15b78a5d31e85788f0ab05af8f90556b
warglaivee <warglaivegm@gmail.com>
2018-05-07 18:40:27 +0300

    up

---
+++ Multidimensional Arrays - Lab/01.SumMatrixElements/Program.SumMatrixElements.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.SumMatrixElements
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            var rows = input[0];
+            var cols = input[1];
+
+            int[][] matrix = new int[rows][];
+
+            for (int i = 0; i < rows; i++)
+            {
+                var elements = Console.ReadLine()
+                    .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse).ToArray();
+                matrix[i] = new int[elements.Length];
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i][j] = elements[j];
+                }
+            }
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(cols);
+            var result = 0;
+            foreach (var currentArray in matrix)
+            {
+                result += currentArray.Sum();
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
